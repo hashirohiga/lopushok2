c95f7d7baa1422e3c681a866b38f550128229ea9
hulkiedo19 <artemmironovhaker@gmail.com>
2022-05-23 11:14:49 +0300

    Добавьте файлы проекта.

---
+++ lopushok2/Command/AddMaterialCommand.cs
@@ -0,0 +1,72 @@
+﻿using lopushok2.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.VisualBasic;
+using lopushok2.Models;
+using System.Windows.Controls;
+
+namespace lopushok2.Command
+{
+    public class AddMaterialCommand :Command
+    {
+        private MaterialsWindowViewModel _viewModel;
+
+        public AddMaterialCommand(MaterialsWindowViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public override void Execute(object parameter)
+        {
+            string title = Interaction.InputBox("Input title", "Filling material row");
+            int countInPack = Convert.ToInt32(Interaction.InputBox("input countInPack", "Filling material row"));
+            string unit = Interaction.InputBox("Input unit", "Filling material row");
+            float countInStock = Convert.ToSingle(Interaction.InputBox("Input countInStock", "Fillint material row"));
+            float minCount = Convert.ToSingle(Interaction.InputBox("Input MinCount", "Filing material row"));
+            decimal cost = Convert.ToDecimal(Interaction.InputBox("Input Cost", "Filling material row"));
+            string description = Interaction.InputBox("Input description", "Filling material row");
+            string image = Interaction.InputBox("Input image", "Filling material row");
+            int materialTypeID = Convert.ToInt32(Interaction.InputBox("Input materialTypeID", "Filling material row"));
+          
+
+            Material material = new Material()
+            {
+                Title = title,
+                MaterialTypeID = materialTypeID,
+                CountInPack = countInPack,
+                Unit = unit,
+                CountInStock = countInStock,
+                MinCount = minCount,
+                Cost = cost,
+                Description = description,
+                Image = image,
+               
+            };
+            using (var DbContext = new DatabaseEntities())
+            {
+                var MaterialType = DbContext.MaterialTypes
+                    .Where(t => t.ID == materialTypeID)
+                    .FirstOrDefault();
+
+                if (MaterialType == default)
+                {
+                    MessageBox.Show("this type of productType doesn't exists, please try again");
+                    return;
+                }
+
+                material.MaterialType = MaterialType;
+
+                DbContext.Materials.Add(material);
+                DbContext.SaveChanges();
+
+                _viewModel.Material.Add(material);
+                (parameter as ItemCollection).Refresh();
+            }
+
+        }
+    }
+}
---
+++ lopushok2/ViewModels/MaterialsWindowViewModel.cs
@@ -0,0 +1,98 @@
+﻿using lopushok2.Command;
+using lopushok2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace lopushok2.ViewModels
+{
+    public class MaterialsWindowViewModel : ViewModel
+    {
+        private string _title;
+        private int _countInPack;
+        private string _unit;
+        private float _countInStock;
+        private float _minCount;
+        private string _description;
+        private decimal _cost;
+        private string _image;
+        private int _materialTypeID;
+
+        private List<Material> _material;
+        private object _selectedMaterial;
+
+        public ICommand AddProduct => new AddMaterialCommand(this);
+        public ICommand DeletepProduct => new DeleteMaterialCommand(this);
+        public ICommand UpdateProduct => new UpdateMaterialCommand(this);
+
+        public MaterialsWindowViewModel()
+        {
+            using (var DbContext = new DatabaseEntities())
+            {
+                _material = DbContext.Materials
+                    .Include(nameof(MaterialType))
+                    .ToList();
+            }
+        }
+
+
+        public List<Material> Material
+        {
+            get => _material;
+            set => Set(ref _material, value, nameof(Material));
+        }
+        public object SelectedMaterial
+        {
+            get => _selectedMaterial;
+            set => Set(ref _selectedMaterial, value, nameof(SelectedMaterial));
+        }
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value, nameof(Title));
+        }
+        public int CountInPack
+        {
+            get => _countInPack;
+            set => Set(ref _countInPack, value, nameof(CountInPack));
+        }
+        public float CountInStock
+        {
+            get => _countInStock;
+            set => Set(ref _countInStock, value, nameof(CountInStock));
+        }
+        public decimal Cost
+        {
+            get => _cost;
+            set => Set(ref _cost, value, nameof(Cost));
+        }
+        public string Image
+        {
+            get => _image;
+            set => Set(ref _image, value, nameof(Image));
+        }
+        public string Unit
+        {
+            get => _unit;
+            set => Set(ref _unit, value, nameof(Unit));
+        }
+        public string Description
+        {
+            get => _description;
+            set => Set(ref _description, value, nameof(Description));
+        }
+        public float MinCount
+        {
+            get => _minCount;
+            set => Set(ref _minCount, value, nameof(MinCount));
+        }
+        public int MaterialTypeID
+        {
+            get => _materialTypeID;
+            set => Set(ref _materialTypeID, value, nameof(MaterialTypeID));
+        }
+    }
+}
